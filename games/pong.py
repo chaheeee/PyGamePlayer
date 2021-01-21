10c2c3c3116f608ab05ad9950295421f0d9ef65d
minm063 <jangmin063@gmail.com>
2021-01-21 19:40:39 +0900

    db

--- games/pong.py
+++ games/pong.py
@@ -299,6 +299,8 @@ while done==False:
         pygame.mixer.Sound.play(pingpong)
         
     if bar2_score == 10:  # ai가 10점 달성시 종료 bgm
+        c.execute("INSERT INTO users('username', 'score, regdate') VALUES(?,?,?)", \
+            ('playern', bar1_score, nowDatetime))
         pygame.mixer.music.stop() 
         pygame.mixer.Sound.play(game_over)
         game_over.set_volume(0.3)
